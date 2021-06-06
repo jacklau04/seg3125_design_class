68e364e50a07a88ca4de56698ab168b0295d8d5a
jacklau <tlau012@uottawa.ca>
2021-06-06 13:23:11 -0700

    update

--- lab3/scripts/groceries.js
+++ lab3/scripts/groceries.js
@@ -1,160 +1,191 @@
 var products = [
-    {
-          name: "Milk",
-          lactose: false,
-          nut: true,
-      organic: false,
-      none: true,
-          price: 2.99,
-      discount: false
-      },
-      {
-          name: "Organic Apple",
-          lactose: true,
-          nut: true,
-      organic: true,
-      none: true,
-          price: 3.99,
-      discount: false
-      },
-      {
-          name: "Organic Banana",
-          lactose: true,
-          nut: true,
-      organic: true,
-      none: true,
-          price: 3.99,
-      discount: false
-      },
-      {
-          name: "Vegetables",
-          lactose: true,
-          nut: true,
-      organic: false,
-      none: true,
-          price: 4.99,
-      discount: false
-      },
-    {
-          name: "Potato Chips",
-          lactose: true,
-          nut: true,
-      organic: false,
-      none: true,
-          price: 4.99,
-      discount: false
-      },
-      {
-          name: "Juice",
-          lactose: true,
-          nut: true,
-      organic: false,
-      none: true,
-          price: 5.99,
-      discount: false
-      },
-    {
-          name: "Bread",
-          lactose: true,
-          nut: true,
-      organic: false,
-      none: true,
-          price: 5.99,
-      discount: false
-      },
-    {
-          name: "Almond Milk",
-          lactose: false,
-          nut: false,
-      organic: false,
-      none: true,
-          price: 5.99,
-      discount: 0.5
-      },
-    {
-          name: "Organic Vegetables",
-          lactose: true,
-          nut: true,
-      organic: true,
-      none: true,
-          price: 7.99,
-      discount: 1
-      },
-      {
-          name: "Organic Nuts",
-          lactose: true,
-          nut: false,
-      organic: true,
-      none: true,
-          price: 9.99,
-      discount: 2
-      },
-    {
-          name: "Mixed Nuts",
-          lactose: true,
-          nut: false,
-      organic: false,
-      none: true,
-          price: 9.99,
-      discount: false
-      },
-    {
-          name: "Salmon",
-          lactose: true,
-          nut: true,
-      organic: false,
-      none: true,
-          price: 19.99,
-      discount: false
+  {
+		name: "Milk",
+		lactose: false,
+		nut: true,
+    organic: false,
+    none: true,
+		price: 2.99,
+    premade: false,
+    discount: false
+	},
+	{
+		name: "Organic Apple",
+		lactose: true,
+		nut: true,
+    organic: true,
+    none: true,
+		price: 3.99,
+    premade: false,
+    discount: false
+	},
+	{
+		name: "Organic Banana",
+		lactose: true,
+		nut: true,
+    organic: true,
+    none: true,
+		price: 3.99,
+    premade: false,
+    discount: false
+	},
+	{
+		name: "Vegetables",
+		lactose: true,
+		nut: true,
+    organic: false,
+    none: true,
+		price: 4.99,
+    premade: false,
+    discount: false
+	},
+  {
+		name: "Potato Chips",
+		lactose: true,
+		nut: true,
+    organic: false,
+    none: true,
+		price: 4.99,
+    premade: false,
+    discount: false
+	},
+	{
+		name: "Juice",
+		lactose: true,
+		nut: true,
+    organic: false,
+    none: true,
+		price: 5.99,
+    premade: false,
+    discount: false
+	},
+  {
+		name: "Bread",
+		lactose: true,
+		nut: true,
+    organic: false,
+    none: true,
+		price: 5.99,
+    premade: false,
+    discount: false
+	},
+  {
+		name: "Almond Milk",
+		lactose: false,
+		nut: false,
+    organic: false,
+    none: true,
+		price: 5.99,
+    premade: false,
+    discount: 0.5
+	},
+  {
+		name: "Organic Vegetables",
+		lactose: true,
+		nut: true,
+    organic: true,
+    none: true,
+		price: 7.99,
+    premade: false,
+    discount: 1
+	},
+	{
+		name: "Organic Nuts",
+		lactose: true,
+		nut: false,
+    organic: true,
+    none: true,
+		price: 9.99,
+    premade: false,
+    discount: 2
+	},
+  {
+		name: "Mixed Nuts",
+		lactose: true,
+		nut: false,
+    organic: false,
+    none: true,
+		price: 9.99,
+    premade: false,
+    discount: false
+	},
+  {
+		name: "Pre-packed Dried Vegetables",
+		lactose: true,
+		nut: true,
+    organic: false,
+    none: true,
+		price: 12.99,
+    premade: true,
+    discount: false
+	},{
+		name: "Pre-packed Chicken Pesto",
+		lactose: true,
+		nut: true,
+    organic: false,
+    none: true,
+		price: 18.99,
+    premade: true,
+    discount: false
+	},
+  {
+		name: "Salmon",
+		lactose: true,
+		nut: true,
+    organic: false,
+    none: true,
+		price: 19.99,
+    premade: false,
+    discount: false
+	}
+];
+	
+
+
+// given restrictions provided, make a reduced list of products
+// prices should be included in this list, as well as a sort based on price
+
+function restrictListProducts(prods, s1, s2, s3, s4, s6) {
+	let product_names = [];
+
+	for (let i=0; i<prods.length; i+=1) {
+		if (!(s1 && !prods[i].lactose) && !(s2 && !prods[i].nut) && !(s3 && !prods[i].organic) && !(s4 && !prods[i].none) && !(s6 && !prods[i].premade)){
+      if (prods[i].discount != false)
+      product_names.push(prods[i].name + " - $" + prods[i].price + " ~~~ save $" + prods[i].discount);
+      else{
+         product_names.push(prods[i].name + " - $" + prods[i].price)
       }
-  ];
-      
-  
-  
-  // given restrictions provided, make a reduced list of products
-  // prices should be included in this list, as well as a sort based on price
-  
-  function restrictListProducts(prods, s1, s2, s3, s4) {
-      let product_names = [];
-  
-      for (let i=0; i<prods.length; i+=1) {
-          if (!(s1 && !prods[i].lactose) && !(s2 && !prods[i].nut) && !(s3 && !prods[i].organic) && !(s4 && !prods[i].none)){
-        if (prods[i].discount != false)
-        product_names.push(prods[i].name + " - $" + prods[i].price + " ~~~ save $" + prods[i].discount);
-        else{
-           product_names.push(prods[i].name + " - $" + prods[i].price)
-        }
-      }
-  
     }
-      return product_names;
+
   }
-  
-  // Calculate the total price of items, with received parameter being a list of products
-  function getTotalPrice(chosenProducts) {
-      totalPrice = 0;
-    for (let j=0; j<chosenProducts.length; j+= 1){
-      for (let i=0; i<products.length; i+=1) {
-        if ((chosenProducts[j] == (products[i].name + " - $" + products[i].price + " ~~~ save $" + products[i].discount)) || (chosenProducts[j] == (products[i].name + " - $" + products[i].price))){
-          totalPrice += products[i].price;
-        }
+	return product_names;
+}
+
+// Calculate the total price of items, with received parameter being a list of products
+function getTotalPrice(chosenProducts) {
+	totalPrice = 0;
+  for (let j=0; j<chosenProducts.length; j+= 1){
+    for (let i=0; i<products.length; i+=1) {
+      if ((chosenProducts[j] == (products[i].name + " - $" + products[i].price + " ~~~ save $" + products[i].discount)) || (chosenProducts[j] == (products[i].name + " - $" + products[i].price))){
+        totalPrice += products[i].price;
       }
     }
-  
-      
-      return totalPrice;
   }
-  
-  function getTotalSave(chosenProducts) {
-      totalPrice = 0;
-    for (let j=0; j<chosenProducts.length; j+= 1){
-      for (let i=0; i<products.length; i+=1) {
-        if ((chosenProducts[j] == (products[i].name + " - $" + products[i].price + " ~~~ save $" + products[i].discount)) || (chosenProducts[j] == (products[i].name + " - $" + products[i].price))){
-          totalPrice += products[i].discount;
-        }
+
+	
+	return totalPrice;
+}
+
+function getTotalSave(chosenProducts) {
+	totalPrice = 0;
+  for (let j=0; j<chosenProducts.length; j+= 1){
+    for (let i=0; i<products.length; i+=1) {
+      if ((chosenProducts[j] == (products[i].name + " - $" + products[i].price + " ~~~ save $" + products[i].discount)) || (chosenProducts[j] == (products[i].name + " - $" + products[i].price))){
+        totalPrice += products[i].discount;
       }
     }
-  
-      
-      return totalPrice;
   }
+
+	
+	return totalPrice;
+}
